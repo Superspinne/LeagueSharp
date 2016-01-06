0a0a6ebef8a5470ef98ceba85b61adf646a65ec1
iMehSharp <imeh.staff@joduska.me>
2016-01-06 04:55:40 +0100

    Orbwalk#
    Uploaded Orbwalk#

---
+++ Walking/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LeagueSharp.Common;
+
+namespace Walking
+{
+    class Program
+    {
+        private static Orbwalking.Orbwalker Orbwalker;
+        private static Menu Menu;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Menu = new Menu("Walker", "Walker", true);
+            Orbwalker = new Orbwalking.Orbwalker(Menu.SubMenu("Orbwalker"));
+
+            Menu.AddToMainMenu();
+        }
+    }
+}
